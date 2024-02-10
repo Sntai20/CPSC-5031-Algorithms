8c29de4867b64cf265edfb69766e50723dfb7d47
Antonio Santana <ansantan@outlook.com>
2024-02-10 13:41:21 -0800

    Add Fib recursive with accumlator placeholder.

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -4,19 +4,6 @@
  * Reference: https://www.c-sharpcorner.com/UploadFile/19b1bd/calculate-fibonacci-series-in-various-ways-using-C-Sharp/
  */
 
-static void FibonacciIterative(int length)
-{
-    int a = 0, b = 1, c = 0;
-    Console.Write("{0} {1}", a, b);
-    for (int i = 2; i < length; i++)
-    {
-        c = a + b;
-        Console.Write(" {0}", c);
-        a = b;
-        b = c;
-    }
-}
-
 static void FibonacciRecursive(int length)
 {
     FibonacciRecursiveTemp(0, 1, 1, length);
@@ -31,8 +18,38 @@ static void FibonacciRecursiveTemp(int a, int b, int counter, int length)
     }
 }
 
-Console.WriteLine("Fibonacci Iterative");
+static void FibonacciIterative(int length)
+{
+    int a = 0, b = 1, c = 0;
+    Console.Write("{0} {1}", a, b);
+    for (int i = 2; i < length; i++)
+    {
+        c = a + b;
+        Console.Write(" {0}", c);
+        a = b;
+        b = c;
+    }
+}
+
+static void FibonacciRecursiveAccumulator(int length)
+{
+    FibonacciRecursiveTemp(0, 1, 1, length);
+}
+
+static void FibonacciRecursiveAccumulatorTemp(int a, int b, int counter, int length)
+{
+    if (counter <= length)
+    {
+        Console.Write("{0} ", a);
+        FibonacciRecursiveAccumulatorTemp(b, a + b, counter + 1, length);
+    }
+}
+
+Console.WriteLine("Fibonacci Recursive");
+FibonacciRecursive(10);
+
+Console.WriteLine("\nFibonacci Iterative");
 FibonacciIterative(10);
 
-Console.WriteLine("\nFibonacci Recursive");
-FibonacciRecursive(10);
+Console.WriteLine("\nFibonacci Recursive with accumulators");
+FibonacciRecursiveAccumulator(10);
