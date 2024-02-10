861abf2cab9ecfc642cdaccf94e2554f88955377
Antonio Santana <ansantan@outlook.com>
2024-02-10 13:06:01 -0800

    Add Fibonacci to the solution.

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,43 +1,36 @@
-/*
+﻿/*
 * Reference: https://www.c-sharpcorner.com/UploadFile/19b1bd/calculate-fibonacci-series-in-various-ways-using-C-Sharp/
 */
-using System;
 
-public class Program
+static void FibonacciIterative(int length)
 {
-	public static void Fibonacci_Iterative(int len)
-	{
-		int a = 0, b = 1, c = 0;
-		Console.Write("{0} {1}", a,b);
-		for (int i = 2; i < len; i++)
-		{
-			c= a + b;
-			Console.Write(" {0}", c);
-			a= b;
-			b= c;
-		}
-	}
-
-    public static void Fibonacci_Recursive(int len)
+    int a = 0, b = 1, c = 0;
+    Console.Write("{0} {1}", a, b);
+    for (int i = 2; i < length; i++)
     {
-        Fibonacci_Rec_Temp(0, 1, 1, len);
+        c = a + b;
+        Console.Write(" {0}", c);
+        a = b;
+        b = c;
     }
-
-    private static void Fibonacci_Rec_Temp(int a, int b, int counter, int len)
-    {
-        if (counter <= len)
-        {
-            Console.Write("{0} ", a);
-            Fibonacci_Rec_Temp(b, a + b, counter+1, len);
-        }
-    }
-	
-	public static void Main()
-	{
-		Console.WriteLine("Fibonacci Iterative");
-		Fibonacci_Iterative(10);
-
-        Console.WriteLine("\nFibonacci Recursive");
-        Fibonacci_Recursive(10);
-	}
 }
+
+static void FibonacciRecursive(int length)
+{
+    FibonacciRecursiveTemp(0, 1, 1, length);
+}
+
+static void FibonacciRecursiveTemp(int a, int b, int counter, int length)
+{
+    if (counter <= length)
+    {
+        Console.Write("{0} ", a);
+        FibonacciRecursiveTemp(b, a + b, counter + 1, length);
+    }
+}
+
+Console.WriteLine("Fibonacci Iterative");
+FibonacciIterative(10);
+
+Console.WriteLine("\nFibonacci Recursive");
+FibonacciRecursive(10);
