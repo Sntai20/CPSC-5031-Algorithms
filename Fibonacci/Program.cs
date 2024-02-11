3fc47650d74e5c000428ece94080309e0a677985
Antonio Santana <ansantan@outlook.com>
2024-02-11 12:44:43 -0800

    Recursive and Iterative are working.

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -4,48 +4,92 @@
  * Reference: https://www.c-sharpcorner.com/UploadFile/19b1bd/calculate-fibonacci-series-in-various-ways-using-C-Sharp/
  */
 
-static void FibonacciRecursive(int length)
+static int FibonacciRecursiveClassic(int numberOfElements)
 {
-    FibonacciRecursiveTemp(0, 1, 1, length);
-}
-
-static void FibonacciRecursiveTemp(int a, int b, int counter, int length)
-{
-    if (counter <= length)
+    Console.Write("{0}", numberOfElements);
+    switch (numberOfElements)
     {
-        Console.Write("{0} ", a);
-        FibonacciRecursiveTemp(b, a + b, counter + 1, length);
+        case 0:
+            //Console.Write("{0}", 0);
+            return 0;
+        case 1:
+            //Console.Write("{0}", 1);
+            return 1;
+        default:
+            //Console.Write("{0} ", ((numberOfElements - 1) + (numberOfElements - 2)));
+            return FibonacciRecursiveClassic(numberOfElements - 1) + FibonacciRecursiveClassic(numberOfElements - 2);
     }
 }
 
-static void FibonacciIterative(int length)
+static void FibonacciRecursive(int numberOfElements)
 {
-    int a = 0, b = 1, c = 0;
-    Console.Write("{0} {1}", a, b);
-    for (int i = 2; i < length; i++)
+    FibonacciRecursiveTemp(0, 1, 1, numberOfElements);
+}
+
+static void FibonacciRecursiveTemp(
+    int firstNumber,
+    int secondNumber,
+    int counter,
+    int numberOfElements)
+{
+    Console.Write(firstNumber + " ");
+    if (counter < numberOfElements)
     {
-        c = a + b;
-        Console.Write(" {0}", c);
-        a = b;
-        b = c;
+        FibonacciRecursiveTemp(
+            secondNumber,
+            firstNumber + secondNumber,
+            counter + 1,
+            numberOfElements);
     }
 }
 
-static void FibonacciRecursiveAccumulator(int length)
+static void FibonacciIterative(int numberOfElements)
 {
-    FibonacciRecursiveTemp(0, 1, 1, length);
-}
+    int firstNumber = 0, secondNumber = 1, nextNumber;
 
-static void FibonacciRecursiveAccumulatorTemp(int a, int b, int counter, int length)
-{
-    if (counter <= length)
+    if (numberOfElements < 2)
     {
-        Console.Write("{0} ", a);
-        FibonacciRecursiveAccumulatorTemp(b, a + b, counter + 1, length);
+        Console.Write("Please Enter a number greater than two");
+    }
+    else
+    {
+        Console.Write("{0} {1}", firstNumber, secondNumber);
+        for (int i = 2; i < numberOfElements; i++)
+        {
+            nextNumber = firstNumber + secondNumber;
+            Console.Write(" {0}", nextNumber);
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
     }
 }
 
-Console.WriteLine("Fibonacci Recursive");
+static void FibonacciRecursiveAccumulator(int numberOfElements)
+{
+    FibonacciRecursiveAccumulatorTemp(0, 1, 1, numberOfElements);
+}
+
+static void FibonacciRecursiveAccumulatorTemp(
+    int firstNumber,
+    int secondNumber,
+    int counter,
+    int numberOfElements)
+{
+    if (counter <= numberOfElements)
+    {
+        Console.Write("{0} ", firstNumber);
+        FibonacciRecursiveAccumulatorTemp(
+            secondNumber,
+            firstNumber + secondNumber,
+            counter + 1,
+            numberOfElements);
+    }
+}
+
+//Console.WriteLine("Fibonacci Recursive Classic");
+//FibonacciRecursiveClassic(10);
+
+Console.WriteLine("\nFibonacci Recursive");
 FibonacciRecursive(10);
 
 Console.WriteLine("\nFibonacci Iterative");
