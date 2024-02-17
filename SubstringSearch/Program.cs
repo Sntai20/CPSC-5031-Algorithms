d1f8f473ab17bb17bd2024522b27ea7e22238bee
Antonio Santana <ansantan@outlook.com>
2024-02-17 11:51:09 -0800

    Working.

--- SubstringSearch/Program.cs
+++ SubstringSearch/Program.cs
@@ -10,10 +10,41 @@
  * I love coding!       Me too! -1
  */
 
-static void substringSearch(string v)
+static int substringSearch(string inputString, string inputSubString)
 {
-    Console.WriteLine("Hello, World! {0}", v);
+    Console.WriteLine("String to search through: {0}", inputString);
+    Console.WriteLine("Substring to search for: {0}", inputSubString);
+
+    int characterMatchCount = 0;
+    for (int i = 0; i < inputString.Length; i++)
+    {
+        // Search for the first occurrence of the substring.
+        if (inputString[i] == inputSubString[0])
+        {
+            // If the substring is found, return the character index where U starts.
+            Console.Write(inputString[i]);
+            characterMatchCount++;
+            for (int j = 1; j < inputSubString.Length; j++)
+            {
+                if (inputString[i + j] == inputSubString[j])
+                {
+                    Console.Write(inputString[i + j]);
+                    characterMatchCount++;
+                }
+            }
+
+            // Return the character index where U starts.
+            if (inputSubString.Length == characterMatchCount)
+            {
+                //Console.WriteLine(inputString.IndexOf(inputString[i]));
+                return inputString.IndexOf(inputString[i]);
+            }
+        }
+    }
+
+    return -1;
 }
 
-string substring = "test";
-substringSearch(substring);
+string inputString = "Happy happy joy joy";
+string inputSubString = "happy";
+Console.WriteLine("\nCharacter Index where U starts: {0}", substringSearch(inputString, inputSubString));
