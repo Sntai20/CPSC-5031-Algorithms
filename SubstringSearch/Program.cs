7c0b96423e83d0479b54994f3e7c0e5595238993
Antonio Santana <ansantan@outlook.com>
2024-02-17 10:24:51 -0800

    .

--- SubstringSearch/Program.cs
+++ SubstringSearch/Program.cs
@@ -1,3 +1,12 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/* Name: Antonio Santana
+ * Problem: Write a function that searches a string S for the first occurrence of substring U. If the substring
+ * is found, return the character index where U starts. If the substring is not found, return -1.
+ */
 
+void substringSearch(string v)
+{
+    Console.WriteLine("Hello, World! {0}", v);
+}
+
+string substring = "test";
+substringSearch(substring);
