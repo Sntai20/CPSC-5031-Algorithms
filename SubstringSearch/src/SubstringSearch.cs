f90012f6d30cf3d460381122edff5006768cb880
Antonio Santana <ansantan@outlook.com>
2024-02-17 18:06:11 -0800

    .

--- SubstringSearch/src/SubstringSearch.cs
+++ SubstringSearch/src/SubstringSearch.cs
@@ -1,15 +1,3 @@
-/* Name: Antonio Santana
- * Problem: Write a function that searches a string S for the first occurrence of substring U. If the substring
- * is found, return the character index where U starts. If the substring is not found, return -1.
- * 
- * Here are the test cases you should include:
- * S                    U       result
- * Happy happy joy joy  happy   6
- * Where is the dog?    cat     -1
- * fun fun fun          fun     0
- * I love coding!       Me too! -1
- */
-
 namespace SubstringSearch;
 
 public class SubstringSearch
